32d0ed3bda5545b2a409baf8089067b68489d6c0
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2013-11-26 18:56:33 -0500

    Fix clean for badly-initialized Upshots.

--- cmd/clean.js
+++ cmd/clean.js
@@ -1,13 +1,15 @@
-var sh = require('shelljs')
+var fs = require('fs')
+  , sh = require('shelljs')
   , config = require('../lib/config')
 
 function clean(argv, options, loader) {
-  if (!config.init) {
-    return
+  if (!fs.existsSync(config.CONFIG_PATH)) {
+    return console.log('Upshot files have already been removed.')
   }
 
   config.clean()
   sh.rm('-rf', config.root)
+  console.log('Upshot files removed.')
 }
 
 module.exports = clean
